8c4bed65def759cadfe67669c0ad88825c5c47b1
durw4rd <misa.fasa@gmail.com>
2016-04-20 12:37:53 +0200

    update counters, toggle, new todo not working

--- script.js
+++ script.js
@@ -1,77 +1,57 @@
 function updateCounters() {
-  var totalCount = document.getElementById('total-count');
-  var totalTodos = document.getElementsByClassName("todo").length;
-  totalCount.innerHTML = totalTodos;
+  var ntodos = $(".todo").length;
+  var ncompleted = $(".completed").length;
 
-  var completedCount = document.getElementById('completed-count');
-  var completedTodos = document.getElementsByClassName("completed").length;
-  completedCount.innerHTML = completedTodos;
-
-  var todoCount = document.getElementById('todo-count');
-  var uncompletedTodos = totalTodos - completedTodos;
-  todoCount.innerHTML = uncompletedTodos;
+  $("#total-count").html(ntodos);
+  $("#completed-count").html(ncompleted);
+  $("#todo-count").html(ntodos - ncompleted);
 }
+
+updateCounters();
 
 function toggleDone() {
   var checkbox = this;
 
-  if (checkbox.checked) {
-    checkbox.parentElement.className = "todo completed";
-  } else {
-    checkbox.parentElement.className = "todo";
-  }
+  $(checkbox).parent().toggleClass("completed");
 
   updateCounters();
 }
 
-function submitTodo() {
-  var inputField = document.getElementById("new-todo");
-  var newTodoTitle = inputField.value;
-  createTodo(newTodoTitle);
+function submitTodo(event) {
+  event.preventDefault();
 
-  inputField.value = null;
+  var title = $("#new-todo").val()
 
+  createTodo(title);
+
+  $("#new-todo").val(null)
   updateCounters();
 }
-
 
 function createTodo(title) {
-  var listItem = document.createElement("li");
-  listItem.className = "todo";
+  var checkboxId = "todo-" + nextTodoId();
 
-  var checkbox = document.createElement("input");
-  checkbox.type = "checkbox";
-  checkbox.id = "todo-" + nextTodoId();
-  checkbox.value = "1";
-  checkbox.checked = false;
-
-  checkbox.onchange = toggleDone.bind(checkbox);
-  listItem.appendChild(checkbox);
-
-  space = document.createTextNode(" ");
-  listItem.appendChild(space);
-
-  var label = document.createElement("label");
-  label.htmlFor = checkbox.id;
-  label.innerHTML = title;
-  listItem.appendChild(label);
-
-  var list = document.getElementById("todolist");
-  list.appendChild(listItem);
+  var newTodo = $("<li></li>")
+      .addClass("todo")
+    .append($('<input>')
+      .attr('type', 'checkbox')
+      .attr('id', checkboxId)
+      .val(1)
+      .bind('change', toggleDone)
+    ).append(
+      document.createTextNode(" ")
+    ).append($('<label></label>')
+      .attr('for', checkboxId)
+      .html(title)
+    );
+  $("#todolist").append( newTodo );
 
   updateCounters();
 }
 
 function nextTodoId() {
-    return document.getElementsByClassName("todo").length + 1;
+  return $(".todo").length + 1;
 }
 
-function cleanUpDoneTodos() {
-  var list = document.getElementById("todolist");
-  var doneItems = document.getElementsByClassName("completed");
-
-  for (var i = doneItems.length; i > 0; i--) {
-    list.removeChild(doneItems[i-1]);
-  }
-  updateCounters();
-}
+$("input[type=checkbox]").bind('change', toggleDone);
+$("form").bind('submit', submitTodo);
